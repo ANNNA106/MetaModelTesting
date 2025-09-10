fdc72f834f797103ff4d672c12fe90022d074ac4
ANNNA106 <132225276+ANNNA106@users.noreply.github.com>
2025-09-10 15:10:26 +0530

    Disable MNIST augmentation image generation

--- generate_mnist_test_data.py
+++ generate_mnist_test_data.py
@@ -10,9 +10,9 @@ from imagegeneration.BasicImageTransformations import transform_images
 aug_output_dir = './datamodels/mnist/data/images/generated_inputs_aug'
 os.makedirs(aug_output_dir, exist_ok=True)
 # Generate all augmentations for MNIST
-for aug_type in ['Rotate', 'Translate', 'Brightness', 'Blur']:
-    print(f"Generating {aug_type} augmented images for MNIST...")
-    transform_images('mnist', aug_type, 2000, aug_output_dir)
+# for aug_type in ['Rotate', 'Translate', 'Brightness', 'Blur']:
+#     print(f"Generating {aug_type} augmented images for MNIST...")
+#     transform_images('mnist', aug_type, 2000, aug_output_dir)
 
 # --- Adversarial Data Generation ---
 from imagegeneration.AdvAttacks_Art import art_attack_cifar
